722d46b7eba3ee4b16e985f5b92ebe02c0e07862
Nelo Puchades <mpuchades@verbio.com>
2021-09-12 21:21:01 +0200

    Finished project

--- packages/client/src/App/App.tsx
+++ packages/client/src/App/App.tsx
@@ -1,24 +1,16 @@
 import * as React from "react";
 
+import {Server} from "../components/Server";
+
 import styles from "./App.module.scss";
 
 const App: React.FC = () => {
   return (
-    <main className={styles.container}>
-      <div className="window" style={{width: 320, margin: "auto"}}>
-        <div className="title-bar">
-          <div className="title-bar-text">Innovid challenge</div>
-          <div className="title-bar-controls">
-            <button aria-label="Minimize" />
-            <button aria-label="Maximize" />
-            <button aria-label="Close" />
-          </div>
-        </div>
-        <div className="window-body">
-          <p>Lets get this party started!</p>
-        </div>
-      </div>
-    </main>
+    <div className={styles.serversContainer}>
+      {[1, 2, 3, 4].map((id) => (
+        <Server key={id} id={id} />
+      ))}
+    </div>
   );
 };
 
---
+++ packages/client/src/components/Server.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import {useState, useRef} from "react";
+
+import styles from "../components/Server.module.scss";
+import pcOff from "../assets/pc-off.png";
+import pcOn from "../assets/pc-on.gif";
+
+type ServerProps = {
+  id: number;
+};
+
+export const Server = ({id}: ServerProps) => {
+  const [isActive, setIsActive] = useState(false);
+  const [serverLoad, setServerLoad] = useState(0);
+  const interval = useRef(null);
+
+  const getServerLoad = async () => {
+    const result = await fetch(`http://localhost:8000/status/${id}`);
+    const response = await result.json();
+
+    setServerLoad(response.load);
+  };
+
+  const handleToggleStatus = () => {
+    if (!isActive) {
+      interval.current = setInterval(() => {
+        getServerLoad();
+      }, 5000);
+    } else {
+      clearInterval(interval.current);
+    }
+    setIsActive(!isActive);
+  };
+
+  return (
+    <main className={styles.serverContainer}>
+      <div className="window" style={{width: 320, margin: "auto"}}>
+        <div className="title-bar">
+          <div className="title-bar-text">Server #{id}</div>
+          <div className="title-bar-controls">
+            <button aria-label="Minimize" />
+            <button aria-label="Maximize" />
+            <button aria-label="Close" />
+          </div>
+        </div>
+        <div className="window-body">
+          <img src={isActive ? pcOn : pcOff} style={{width: 150}} />
+        </div>
+        <div className="status-bar">
+          <p className="status-bar-field">
+            Status:{" "}
+            <span className={isActive ? styles.active : styles.notActive}>
+              {isActive ? "ON" : "OFF"}
+            </span>
+          </p>
+          <a
+            className="status-bar-field"
+            style={{cursor: "pointer"}}
+            onClick={() => handleToggleStatus()}
+          >
+            turn {isActive ? "off" : "on"}
+          </a>
+          <p className="status-bar-field">CPU Usage: {serverLoad}%</p>
+        </div>
+      </div>
+    </main>
+  );
+};
