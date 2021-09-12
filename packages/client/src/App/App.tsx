ee228a00b120e63ea75af14640f479bb7235065d
Nelo Puchades <mpuchades@verbio.com>
2021-09-12 22:46:04 +0200

    Added button to add more servers

--- packages/client/src/App/App.tsx
+++ packages/client/src/App/App.tsx
@@ -1,16 +1,32 @@
 import * as React from "react";
+import {useState} from "react";
 
 import {Server} from "../components/Server";
 
 import styles from "./App.module.scss";
 
 const App: React.FC = () => {
+  const [numberOfServers, setNumberOfServers] = useState(4);
+
+  const getArrayOfGivenElements = (numberOfElements: number) => {
+    return Array.from({length: numberOfElements}, (_, i) => i + 1);
+  };
+
+  const addServer = () => {
+    setNumberOfServers(numberOfServers + 1);
+  };
+
   return (
-    <div className={styles.serversContainer}>
-      {[1, 2, 3, 4].map((id) => (
-        <Server key={id} id={id} />
-      ))}
-    </div>
+    <>
+      <button className={styles.addServer} onClick={addServer}>
+        Add server
+      </button>
+      <div className={styles.serversContainer}>
+        {getArrayOfGivenElements(numberOfServers).map((id) => (
+          <Server key={id} id={id} />
+        ))}
+      </div>
+    </>
   );
 };
 
